9a4e7505fef56e9fc4248d37beadeb9953355aa6
wendeehsu <b05705046@ntu.edu.tw>
2023-10-22 21:04:27 -0500

    feat: support two rings

--- arduino/rasengan/rasengan.ino
+++ arduino/rasengan/rasengan.ino
@@ -4,12 +4,19 @@
 #endif
 
 #define PIN 6
+#define PIN_s 4
+#define LIGHT_NUM_L 24
+#define LIGHT_NUM_S 16
 
-Adafruit_NeoPixel strip = Adafruit_NeoPixel(24, PIN, NEO_GRB + NEO_KHZ800);
+Adafruit_NeoPixel strip = Adafruit_NeoPixel(LIGHT_NUM_L, PIN, NEO_GRB + NEO_KHZ800);
+Adafruit_NeoPixel strip_s = Adafruit_NeoPixel(LIGHT_NUM_S, PIN_s, NEO_GRB + NEO_KHZ800);
 
 const int buttonPin = 2;     // THIS IS THE VAlUE YOU COULD CHANGE TO 3
 int buttonState = 0;         // variable for reading the pushbutton status
 int rainbow_color = 1;
+int lights[LIGHT_NUM_L];
+int lights_s[LIGHT_NUM_S];
+bool hasBlink = false;
 
 void setup() {
   // This is for Trinket 5V 16MHz, you can remove these three lines if you are not using a Trinket
@@ -21,14 +28,31 @@ void setup() {
   strip.begin();
   strip.setBrightness(50);
   strip.show(); // Initialize all pixels to 'off'
-  pinMode(buttonPin, INPUT);
-}
 
-bool hasBlink = false;
+  strip_s.begin();
+  strip_s.setBrightness(50);
+  strip_s.show(); // Initialize all pixels to 'off'
+  pinMode(buttonPin, INPUT);
+
+  for (int i = 0; i < strip.numPixels(); i++) {
+    lights[i] = i;
+  }
+
+  for (int i = 0; i < LIGHT_NUM_S; i++) {
+    lights_s[i] = LIGHT_NUM_S - i -1;
+  }
+}
 
 void loop() {
   buttonState = digitalRead(buttonPin);
   if (buttonState == LOW && !hasBlink) {
+    // turn off the small ring
+    for (int i = 0; i < LIGHT_NUM_S; i++) {
+      strip_s.setPixelColor(lights_s[i], 0,0,0);
+    }
+    strip_s.show();
+
+    // blink the large ring
     for (int i = 0; i < 10; i++) {
       blink(30);
     }
@@ -38,42 +62,65 @@ void loop() {
   if (buttonState == LOW) {
       cycle(30);
   } else {
-    rainbow(20, rainbow_color);
-    rainbow_color += 1;
-    if (rainbow_color > 256) {
-      rainbow_color = 1;
-    }
     hasBlink = false;
   }
 }
 
 void blink(int wait) {
+  int r = random(10,255);
+  int g = random(10,255);
+  int b = random(10,255);
   for(uint16_t i=0; i<strip.numPixels(); i++) {
-    strip.setPixelColor(i, 0, 0, 0);
+    strip.setPixelColor(i, r, g, b);
   }
   strip.show();
   delay(wait+5);
 
   for(uint16_t i=0; i<strip.numPixels(); i++) {
-    strip.setPixelColor(i, 255, 255, 255);
+    strip.setPixelColor(i, 0,0,0);
   }
   strip.show();
   delay(wait);
 }
 
 void cycle(int wait) {
+  // large ring
+  int gap = strip.numPixels()/2;
   for (int i = 0; i < strip.numPixels()/2; i++) {
-    strip.setPixelColor(i, 194, 255, 248);
-    strip.setPixelColor(i+strip.numPixels()/2, 194, 255, 248);
-    strip.show();
-    delay(wait);
+    strip.setPixelColor(lights[i], 40 - i * 40/gap, 211 - i * 211/gap, 255 - i * 255/gap);
   }
-  for (int i = 0; i < strip.numPixels()/2; i++) {
-    strip.setPixelColor(i, 10, 10, 10);
-    strip.setPixelColor(i+strip.numPixels()/2, 10,10,10);
-    strip.show();
-    delay(wait);
+
+  for (int i = strip.numPixels()/2; i < strip.numPixels(); i++) {
+    strip.setPixelColor(lights[i], 0,0,0);
   }
+  strip.show();
+
+  // small ring
+  int gap_s = strip_s.numPixels()/2;
+  for (int i = 0; i < strip_s.numPixels()/2; i++) {
+    strip_s.setPixelColor(lights_s[i], 255 - i * 255/gap_s, 255 - i * 255/gap_s, 255 - i * 255/gap_s);
+  }
+
+  for (int i = strip_s.numPixels()/2; i < strip_s.numPixels(); i++) {
+    strip_s.setPixelColor(lights_s[i], 0,0,0);
+  }
+  strip_s.show();
+
+  delay(wait);
+
+  // update large lights
+  int tmp = lights[0];
+  for (int i = 0; i < strip.numPixels()-1; i++) {
+    lights[i] = lights[i+1];
+  }
+  lights[strip.numPixels()-1] = tmp;
+
+   // update small lights
+  int tmp_s = lights_s[0];
+  for (int i = 0; i < strip_s.numPixels()-1; i++) {
+    lights_s[i] = lights_s[i+1];
+  }
+  lights_s[strip_s.numPixels()-1] = tmp_s;
 }
 
 // Fill the dots one after the other with a color
